c8011eb614d8a3c37c56509b5caafe4282883e5c
Rongali Vamsi <rongalivamsi@Rongalis-MacBook-Air.local>
2025-07-07 00:14:25 +0530

    added another iamge

--- .next/static/chunks/src_55a91b04._.js
+++ .next/static/chunks/src_55a91b04._.js
@@ -1154,22 +1154,22 @@ function Gallery() {
     const items = [
         {
             title: "Tyler Durden",
-            image: "https://images.unsplash.com/photo-1732310216648-603c0255c000?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "/svg/AVASA.svg",
             className: "absolute top-20 left-10 rotate-[-12deg] sm:top-16 sm:left-[15%] md:top-20 md:left-[20%]"
         },
         {
             title: "The Narrator",
-            image: "https://images.unsplash.com/photo-1697909623564-3dae17f6c20b?q=80&w=2667&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "/svg/AVASA.svg",
             className: "absolute top-20 right-10 rotate-[15deg] sm:top-16 sm:right-[15%] md:top-20 md:right-[20%]"
         },
         {
             title: "Iceland",
-            image: "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "/svg/AVASA.svg",
             className: "absolute bottom-20 left-10 rotate-[-8deg] sm:bottom-16 sm:left-[15%] md:bottom-20 md:left-[20%]"
         },
         {
             title: "Japan",
-            image: "https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "/svg/AVASA.svg",
             className: "absolute bottom-20 right-10 rotate-[20deg] sm:bottom-16 sm:right-[15%] md:bottom-20 md:right-[20%]"
         }
     ];
@@ -1181,7 +1181,7 @@ function Gallery() {
                 children: "GALLERY"
             }, void 0, false, {
                 fileName: "[project]/src/components/utils/gallerySection/Gallery.jsx",
-                lineNumber: 214,
+                lineNumber: 211,
                 columnNumber: 7
             }, this),
             items.map((item, idx)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(DraggableCardBody, {
@@ -1193,7 +1193,7 @@ function Gallery() {
                             className: "z-10 object-cover relative h-60 w-60 w-80 pointer-events-none md:h-80"
                         }, void 0, false, {
                             fileName: "[project]/src/components/utils/gallerySection/Gallery.jsx",
-                            lineNumber: 225,
+                            lineNumber: 222,
                             columnNumber: 11
                         }, this),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("h3", {
@@ -1201,19 +1201,19 @@ function Gallery() {
                             children: item.title
                         }, void 0, false, {
                             fileName: "[project]/src/components/utils/gallerySection/Gallery.jsx",
-                            lineNumber: 230,
+                            lineNumber: 227,
                             columnNumber: 11
                         }, this)
                     ]
                 }, item.title + idx, true, {
                     fileName: "[project]/src/components/utils/gallerySection/Gallery.jsx",
-                    lineNumber: 218,
+                    lineNumber: 215,
                     columnNumber: 9
                 }, this))
         ]
     }, void 0, true, {
         fileName: "[project]/src/components/utils/gallerySection/Gallery.jsx",
-        lineNumber: 213,
+        lineNumber: 210,
         columnNumber: 5
     }, this);
 }
--- src/components/utils/gallerySection/Gallery.jsx
+++ src/components/utils/gallerySection/Gallery.jsx
@@ -182,29 +182,26 @@ export function Gallery() {
   const items = [
     {
       title: "Tyler Durden",
-      image:
-        "https://images.unsplash.com/photo-1732310216648-603c0255c000?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "/svg/AVASA.svg", // Replace with your actual image path
       className:
         "absolute top-20 left-10 rotate-[-12deg] sm:top-16 sm:left-[15%] md:top-20 md:left-[20%]",
     },
+
     {
       title: "The Narrator",
-      image:
-        "https://images.unsplash.com/photo-1697909623564-3dae17f6c20b?q=80&w=2667&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "/svg/AVASA.svg",
       className:
         "absolute top-20 right-10 rotate-[15deg] sm:top-16 sm:right-[15%] md:top-20 md:right-[20%]",
     },
     {
       title: "Iceland",
-      image:
-        "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "/svg/AVASA.svg",
       className:
         "absolute bottom-20 left-10 rotate-[-8deg] sm:bottom-16 sm:left-[15%] md:bottom-20 md:left-[20%]",
     },
     {
       title: "Japan",
-      image:
-        "https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "/svg/AVASA.svg",
       className:
         "absolute bottom-20 right-10 rotate-[20deg] sm:bottom-16 sm:right-[15%] md:bottom-20 md:right-[20%]",
     },
