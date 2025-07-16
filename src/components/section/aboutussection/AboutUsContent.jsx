f44e992d6ac1527a47c702c3a9ac74f5cd8d2b0e
Rongali Vamsi <rongalivamsi@Rongalis-MacBook-Air.local>
2025-07-16 10:37:51 +0530

    added vinathi work

--- src/components/section/aboutussection/AboutUsContent.jsx
+++ src/components/section/aboutussection/AboutUsContent.jsx
@@ -21,13 +21,13 @@ export default function AboutUsContent() {
     {
       name: "V. Radha Priyanka",
       role: "Founder/ President",
-      desc: "Visionary and compassionate, she leads Avasa with purpose, heart, and unwavering commitment to its mission."
+      desc: "Visionary and compassionate, she leads Avasa with purpose, heart, and unwavering commitment to its mission.",
       img: "/team/vinathi.jpg",
     },
     {
       name: "V. Keerthi Sri",
       role: "Vice President",
-      desc: "Provides strategic oversight, ensuring organizational alignment, operational excellence, and effective implementation of programs."
+      desc: "Provides strategic oversight, ensuring organizational alignment, operational excellence, and effective implementation of programs.",
       img: "/team/vinathi.jpg",
     },
     {
@@ -120,34 +120,6 @@ export default function AboutUsContent() {
         viewport={{ once: true }}
         variants={fadeIn}
       >
-
-      <div className="flex justify-around items-center bg-[#fb64b6]/10 rounded-xl shadow-md hover:shadow-lg transition">
-      <div className="flex-1/2">
-        <h2 className="mb-6 text-3xl font-bold text-[#fb64b6] text-center sm:text-4xl">
-          Who We Are
-        </h2>
-        <p className="mx-auto px-2   text-center leading-relaxed text-white text-lg sm:px-14">
-          What began as a heartfelt initiative by{" "}
-          <strong className="text-[#fb64b6]">V. Radha Priyanka</strong> and her
-          close friends in 2022 has now grown into a registered movement of
-          compassion and change. Rooted in Hyderabad, our journey started with
-          simple acts—distributing clothes and food to those in need.
-          <br />
-          <br />
-          As our vision expanded, so did our reach. We built a dedicated team
-          and extended to Jaipur—taking our mission of care to more communities.
-          From refreshment drives and medical kit distributions to visits to
-          orphanages and old age homes, every effort is driven by the same
-          purpose—to bring hope, dignity, and support.
-          <br />
-          <br />
-          Founded in 2022 and officially incorporated as a <strong className="text-[#fb64b6]">registered NGO in November 2024</strong>, 
-          Avasa is registered under the Telangana Societies Registration Act, 2001 
-          with registration number <strong className="text-[#fb64b6]">1108 of 2024</strong>. This recognition enables us to scale our
-          initiatives and deepen our commitment to meaningful, community-driven change.
-        </p>
-      </div>
-
         <div className="flex justify-around items-center bg-[#fb64b6]/10 rounded-xl shadow-md hover:shadow-lg transition">
           <div className="flex-1/2">
             <h2 className="mb-6 text-3xl font-bold text-[#fb64b6] text-center sm:text-4xl">
@@ -168,11 +140,17 @@ export default function AboutUsContent() {
               by the same purpose—to bring hope, dignity, and support.
               <br />
               <br />
-              In November 2024, we officially registered as an NGO, allowing us
-              to amplify our impact and reach.
+              Founded in 2022 and officially incorporated as a{" "}
+              <strong className="text-[#fb64b6]">
+                registered NGO in November 2024
+              </strong>
+              , Avasa is registered under the Telangana Societies Registration
+              Act, 2001 with registration number{" "}
+              <strong className="text-[#fb64b6]">1108 of 2024</strong>. This
+              recognition enables us to scale our initiatives and deepen our
+              commitment to meaningful, community-driven change.
             </p>
           </div>
-
           <Image
             className="m-10 rounded-2xl"
             width={550}
@@ -247,23 +225,23 @@ export default function AboutUsContent() {
         whileInView="visible"
         viewport={{ once: true }}
         variants={fadeIn}
-        className="relative"
+        className="relative min-h-[380vh] sm:min-h-[200vh] md:min-h-[130vh] lg:min-h-[140vh]"
       >
-        <div className="mt-16 sm:mt-20 md:mt-24 mx-20 lg:mt-32">
+        <div className="mt-16 mx-20 sm:mt-20 md:mt-24 lg:mt-32">
           <h2 className="text-3xl font-bold text-[#fb64b6] text-center sm:text-4xl">
             Meet Our Board
           </h2>
 
-          <div className="flex flex-wrap justify-center gap-6 mt-8 px-2 sm:gap-8 mt-10 px-0">
+          <div className="flex flex-wrap justify-center gap-6 mt-8 px-2 mt-10 px-0 sm:gap-8">
             {team.map((member, index) => (
               <div
                 key={index}
-                className="p-5 w-[85vw] text-center bg-[#fb64b6]/10 rounded-xl shadow-md transition-transform duration-300 hover:scale-105 hover:shadow-lg sm:p-6 w-[280px] md:w-[300px] lg:w-[320px]"
+                className="p-5 w-[85vw] w-[280px] text-center bg-[#fb64b6]/10 rounded-xl shadow-md transition-transform duration-300 hover:scale-105 hover:shadow-lg sm:p-6 md:w-[300px] lg:w-[320px]"
               >
                 <img
                   src={member.img}
                   alt={member.name}
-                  className="object-cover mx-auto mb-4 w-24 h-24 rounded-full border-4 border-[#fb64b6] sm:h-28 w-28"
+                  className="object-cover mx-auto mb-4 w-24 h-24 w-28 rounded-full border-4 border-[#fb64b6] sm:h-28"
                 />
                 <h3 className="text-lg font-semibold text-[#fb64b6] sm:text-xl">
                   {member.name}
