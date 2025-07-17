77fb162306388f76a05e940878747494b404443c
Rongali Vamsi <rongalivamsi@Rongalis-MacBook-Air.local>
2025-07-17 16:55:18 +0530

    upcoming events

--- src/app/ourwork/upcomingevents/page.js
+++ src/app/ourwork/upcomingevents/page.js
@@ -1,9 +1,11 @@
 import EventUpdatesPage from "@/components/section/ourworksection/upcomingevents/EventUpdatesPage";
+import FlyingBirdLogo from "@/components/utils/avasalogo/FlyingBirdLogo";
 
 export default function UpcomingEventsPage() {
   return (
     <div>
-      <EventUpdatesPage />
+      {/* <EventUpdatesPage /> */}
+      <FlyingBirdLogo />
     </div>
   );
 }
