7a9610d97c94fd471dc5a6af60c956fcbb4bb725
Rongali Vamsi <rongalivamsi@Rongalis-MacBook-Air.local>
2025-07-21 23:32:43 +0530

    added font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/common/navbar/Navbar";
 import Footer from "@/components/common/footer/Footer";
 import AnimatedCursor from "@/components/utils/animatedcursor/AnimatedCursor";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Avasa Foundation",
@@ -25,9 +14,16 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/svg/AVASA.svg" type="image/svg+xml" />
+        <style>
+          {`
+            body {
+              font-family: 'Times New Roman', Times, serif !important;
+            }
+          `}
+        </style>
       </head>
       <body
-        className={`min-h-screen flex flex-col antialiased relative ${geistSans.variable} ${geistMono.variable}`}
+        className="min-h-screen flex flex-col antialiased relative"
         style={{ backgroundColor: "black" }}
       >
         <NavBar />
