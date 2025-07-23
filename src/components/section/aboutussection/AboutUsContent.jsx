882b7a31691f223410f18f07f72f89001f9833b6
Rongali Vamsi <rongalivamsi@Rongalis-MacBook-Air.local>
2025-07-23 23:08:20 +0530

    aboutus

--- src/components/section/aboutussection/AboutUsContent.jsx
+++ src/components/section/aboutussection/AboutUsContent.jsx
@@ -93,34 +93,38 @@ export default function AboutUsContent() {
           <h2 className="mb-6 text-3xl font-semibold text-transparent bg-pink-300 bg-clip-text sm:text-4xl">
             Who We Are
           </h2>
-         <div className="space-y-4">
-          <p>
-            What began as a heartfelt initiative by{" "}
-            <strong className="text-[#fb64b6]">V. Radha Priyanka</strong> and
-            her close friends in 2022 has now grown into a registered movement
-            of compassion and change. Rooted in Hyderabad, our journey started
-            with simple acts—distributing clothes and food to those in need.
-          </p>
-        
-          <p>
-            As our vision expanded, so did our reach. We built a dedicated team
-            and extended to Jaipur—taking our mission of care to more
-            communities. From refreshment drives and medical kit distributions
-            to visits to orphanages and old age homes, every effort is driven by
-            the same purpose—to bring hope, dignity, and support.
-          </p>
-        
-          <p>
-            Founded in 2022 and officially incorporated as a{" "}
-            <strong className="text-[#fb64b6]">registered NGO in November 2024</strong>,
-            Avasa is registered under the{" "}
-            <strong className="text-[#fb64b6]">Telangana Societies Registration Act, 2001</strong>{" "}
-            with registration number{" "}
-            <strong className="text-[#fb64b6]">1108 of 2024</strong>. This
-            recognition enables us to scale our initiatives and deepen our
-            commitment to meaningful, community-driven change.
-          </p>
-        </div>
+          <div className="space-y-4">
+            <p>
+              What began as a heartfelt initiative by{" "}
+              <strong className="text-[#fb64b6]">V. Radha Priyanka</strong> and
+              her close friends in 2022 has now grown into a registered movement
+              of compassion and change. Rooted in Hyderabad, our journey started
+              with simple acts—distributing clothes and food to those in need.
+            </p>
+
+            <p>
+              As our vision expanded, so did our reach. We built a dedicated
+              team and extended to Jaipur—taking our mission of care to more
+              communities. From refreshment drives and medical kit distributions
+              to visits to orphanages and old age homes, every effort is driven
+              by the same purpose—to bring hope, dignity, and support.
+            </p>
+
+            <p>
+              Founded in 2022 and officially incorporated as a{" "}
+              <strong className="text-[#fb64b6]">
+                registered NGO in November 2024
+              </strong>
+              , Avasa is registered under the{" "}
+              <strong className="text-[#fb64b6]">
+                Telangana Societies Registration Act, 2001
+              </strong>{" "}
+              with registration number{" "}
+              <strong className="text-[#fb64b6]">1108 of 2024</strong>. This
+              recognition enables us to scale our initiatives and deepen our
+              commitment to meaningful, community-driven change.
+            </p>
+          </div>
         </div>
         <div className="overflow-hidden relative w-full h-64 rounded-xl sm:h-96">
           <Image
