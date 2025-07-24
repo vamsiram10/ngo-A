a0521495335eaa30e0c24715205471b207fffdc0
Rongali Vamsi <rongalivamsi@Rongalis-MacBook-Air.local>
2025-07-24 12:20:00 +0530

    volunteering

--- src/components/section/aboutussection/AboutUsContent.jsx
+++ src/components/section/aboutussection/AboutUsContent.jsx
@@ -27,7 +27,7 @@ export default function AboutUsContent() {
       name: "V. Keerthi Sri",
       role: "Vice President",
       desc: "Provides strategic oversight, ensuring organizational alignment, operational excellence, and effective implementation of programs.",
-      img: "/meet/Keerti3.jpg",
+      img: "/meet/keerthi.jpg",
     },
     {
       name: "Ananya Pandey",
