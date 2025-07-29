322443e2b4a8b20ba4d59528f96e6143e792d655
Rongali Vamsi <rongalivamsi@Rongalis-MacBook-Air.local>
2025-07-29 23:39:31 +0530

    photos

--- src/app/ourwork/page.js
+++ src/app/ourwork/page.js
@@ -1,7 +1,4 @@
-import Events from "@/components/section/ourworksection/pastevents/Events";
 import OurWorkSection from "@/components/section/ourworksection/OurWorkSection";
-import GallerySection from "@/components/utils/gallery/GallerySection";
-import MapLocation from "@/components/section/contactussection/maplocation/MapLocation";
 
 const OurWorkPage = () => {
   return (
